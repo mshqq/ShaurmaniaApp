19b6fd5f32cdf302a27a709109fdb901bb6982c7
mshqq <miahasik@gmail.com>
2026-07-17 04:12:43 +0900

    Обновлена модель пользователя
    - Добавлены функции хэширования при создании пользователя и функция про-
    верки соответствия пароля хэшу из базы

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app.extensions import db
 from datetime import datetime
 from uuid import uuid4
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Location(db.Model):
@@ -106,3 +107,9 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<{self.id}:{self.username}>"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
