d656043df6a6573b497203fa91167c77cdd48340
mshqq <miahasik@gmail.com>
2026-07-17 03:37:37 +0900

    Debug, host, port убраны в переменные окружения
    - app.py: значения debug, host, port вынесены в .env. Пример представлен
    в файле .envexample
    - app.py: при старте значение порта валидируется 1-65535
    - app.py: при отсутствии значений приложение запустится в базовой конфи-
    гурации

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from app import create_app
+from dotenv import load_dotenv
+from os import getenv
+from sys import exit
+
+load_dotenv()
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    debug = getenv("FLASK_DEBUG", "0") == "1"
+    host = getenv("FLASK_HOST", "127.0.0.1")
+    port = int(getenv("FLASK_PORT", 5000))
+
+    if not (1 <= port <= 65535):
+        exit(f"FLASK_PORT={port} вне диапазона 1-65535")
+
+    app.run(
+        debug=debug,
+        host=host,
+        port=port,
+    )
