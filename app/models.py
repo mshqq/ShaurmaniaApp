ab13f4e6cedebba94776804f64b5b50e13afd611
mshqq <miahasik@gmail.com>
2026-07-17 04:06:10 +0900

    Добавлена модель пользователя

--- app/models.py
+++ app/models.py
@@ -94,3 +94,15 @@ class Subscriber(db.Model):
 
     def __repr__(self):
         return f"<Subscribers {self.email}>"
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(100), nullable=False, unique=True)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<{self.id}:{self.username}>"
